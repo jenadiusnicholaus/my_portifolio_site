134d9f2732e1aa9604a2e98ad80fd8c0957edc66
jenadiusnicholaus <ucode888@gmail.com>
2022-03-09 23:42:26 +0300

    Update added javascripts to the modal when user clicks on button

---
+++ assets/js/modals.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById('project-modal');
+const btn = document.getElementById('myBtn');
+
+const span = document.getElementsByClassName('close')[0];
+
+btn.onclick = () => {
+  modal.style.display = 'block';
+};
+
+span.onclick = () => {
+  modal.style.display = 'none';
+};
+
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
