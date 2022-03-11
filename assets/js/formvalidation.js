9e656159f3f69b9687c3825e343cb8b6fa2879a3
jenadiusnicholaus <ucode888@gmail.com>
2022-03-11 19:24:57 +0300

    Add storage

--- assets/js/formvalidation.js
+++ assets/js/formvalidation.js
@@ -1,6 +1,14 @@
 document.getElementById('contactform').onsubmit = () => {
+  const fullName = document.getElementById('full-name').value;
   const email = document.getElementById('email').value;
+  const contents = document.getElementById('contents').value;
   if (email === email.toLowerCase()) {
+    const allFields = {
+      full_name: fullName,
+      user_email: email,
+      user_contents: contents
+    }
+    localStorage.setItem('contacts_info', JSON.stringify(allFields));
     return true;
   }
   const messege = document.getElementById('message-div');
@@ -8,3 +16,4 @@ document.getElementById('contactform').onsubmit = () => {
   messege.textContent = 'Email must be in lower case';
   return false;
 };
+
