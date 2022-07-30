a9b8500cd2f524b8e054dc68a0a5fe4e2863c6b8
jenadiusnicholaus <ucode888@gmail.com>
2022-07-30 23:57:22 +0300

    modify project list using api

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,7 +3,7 @@ class Api {
        const headers = new Headers();
        headers.append('Content-Type', 'application/json');
        const requestOptions = { method: 'GET', redirect: 'follow', headers };
-       const response = await fetch('http://jenadiusnicholausportifolio.pythonanywhere.com/api/projects/', requestOptions);
+       const response = await fetch('https://jenadiusnicholausportifolio.pythonanywhere.com/api/projects/', requestOptions);
        try {
          if (response.ok) {
            const decodedResponse = await response.json();
