c7ceaf69d7236cee0e576c8b5c40a0d122178e41
jenadiusnicholaus <ucode888@gmail.com>
2022-07-30 23:49:34 +0300

    modify project list using api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,6 @@
 import Api from './api.js';
 
-const projectData = [
-  {
-    id: 0,
-    name: 'Tv Show Project',
-    description: 'This is a TV Shows web site source from TVmaze API. In the website, a user can click on heart icon and get it recorded in Involvement API. Similarly, a user can write comment and get it recorded in Involvement API.',
-    featured: 'jenad',
-    image: 'assets/img/tvshow.png',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascripts',
-      'jest',
-    ],
-    link_to_live_version: 'https://jenadiusnicholaus.github.io/Leaderboard/',
-    link_to_source: 'https://github.com/jenadiusnicholaus/Leaderboard',
-  },
 
-  {
-    id: 1,
-    name: 'Lead Boarder',
-    description: 'The leaderboard website displays scores submitted by different players, it meant a play gets unique id for the Game session and from there can be allowed to play the game and to do that i used Leaderboard API using JavaScript async and await and add simple styling.',
-    featured: 'jenad',
-    image: 'assets/img/leaderBoard.png',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascripts',
-      'jest',
-    ],
-    link_to_live_version: 'https://jenadiusnicholaus.github.io/Leaderboard/',
-    link_to_source: 'https://github.com/jenadiusnicholaus/Leaderboard',
-  },
-
-  {
-    id: 2,
-    name: 'To Do List Project',
-    description: '"To-do list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete. I will build a simple website that allows for doing that, and i will do it using ES6 and Webpack!',
-    featured: 'jenad',
-    image: 'assets/img/mytodo2.png',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascripts',
-      'jest',
-    ],
-    link_to_live_version: 'https://jenadiusnicholaus.github.io/To-Do-list/',
-    link_to_source: 'https://github.com/jenadiusnicholaus/To-Do-list',
-  },
-
-  {
-    id: 3,
-    name: 'Wora',
-    description: 'To-do list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete. I will build a simple website that allows for doing that, and i will do it using ES6 and Webpack!',
-    featured: 'jenad',
-    image: 'assets/img/wora.png',
-    technologies: [
-      'flutter',
-      'Dart',
-      'firebase',
-    ],
-    link_to_live_version: 'https://jenadiusnicholaus.github.io/To-Do-list/',
-    link_to_source: 'https://github.com/jenadiusnicholaus/To-Do-list',
-  },
-
-];
 const logoToggleClass = (element, toggleClass) => {
   element.classList.toggle(toggleClass);
 };
@@ -77,7 +13,7 @@ document.querySelectorAll('.hamburger-mobile-icon').forEach((hamburgIcon) => {
   });
 });
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
   const workCardContainer = document.getElementById('work-card-container');
   const recentworktitle = document.getElementById('recentworktitle');
   const recentworkdesc = document.getElementById('recentworkdesc');
@@ -87,35 +23,38 @@ window.addEventListener('load', () => {
   const techThree = document.getElementById('tech-3');
   workCardContainer.innerHTML = '';
   let htmlCard = '';
+  const projectData  = await Api.getProjects();
 
   // Update recent project dynamicaly
   recentworktitle.textContent = projectData[0].name;
   img.setAttribute('src', projectData[0].image);
   recentworkdesc.textContent = projectData[0].description;
-  [techOne.value, techTwo.value, techThree.value] = projectData[0].technologies;
+  // [techOne.value, techTwo.value, techThree.value] = projectData[0].technologies;
 
-  // get data from storage
+  // get data from  local storage
   const retrievedObject = localStorage.getItem('contacts_info');
   const parseJson = JSON.parse(retrievedObject);
   // document.getElementById('full-name').value = parseJson.full_name ? parseJson.full_name : '';
   // document.getElementById('email').value = parseJson.user_email ? parseJson.user_email : '';
   // document.getElementById('contents').value = parseJson.user_contents ? parseJson.user_contents : '';
 
-  for (let index = 0; index < projectData.length; index += 1) {
-    const project = projectData[index];
+
+  for (let index = 0; index <  projectData.length; index += 1) {
+    const project =  projectData[index];
+    console.log(project)
 
     htmlCard += `<div class="card-container">
     <div class="work-card">
         <div class="work-card-body">
-            <h1>${project.name}</h1>
+            <h1>${project.title}</h1>
             <p class="work-description">
             ${project.description}
             </p>
             <div class="work-tech-stack">
                 <ul class="ul-links">
-                <li class="t-stack"><a type="button" class="btn-tech-stacks">${project.technologies[0]}</a></li>
-                <li class="t-stack"><a type="button" class="btn-tech-stacks">${project.technologies[1]}</a></li>
-                <li class="t-stack"><a type="button" class="btn-tech-stacks">${project.technologies[2]}</a></li>
+                <li class="t-stack"><a type="button" class="btn-tech-stacks">${project.technologies[0].name}</a></li>
+                <li class="t-stack"><a type="button" class="btn-tech-stacks">${project.technologies[1].name}</a></li>
+                <li class="t-stack"><a type="button" class="btn-tech-stacks">${project.technologies[2].name}</a></li>
                 </ul>
             </div>
         </div>
@@ -130,4 +69,3 @@ window.addEventListener('load', () => {
   const recent_works_card = document.querySelector('.work-card');
   // recent_works_card.style.background= "linear-gradient(180.45deg, rgba(38, 38, 38, 0) 0.75%, rgba(34, 32, 32, 0.184) 61.94%), url('./img/maskimage.png')";
 });
-Api.getProjects();
--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,76 +1,12 @@
-const projectDataTwo = [
-  {
-    id: 0,
-    name: 'Tv Show Project',
-    description: 'This is a TV Shows web site source from TVmaze API. In the website, a user can click on heart icon and get it recorded in Involvement API. Similarly, a user can write comment and get it recorded in Involvement API.',
-    featured: 'jenad',
-    image: 'assets/img/tvshow.png',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascripts',
-      'jest',
-    ],
-    link_to_live_version: 'https://jenadiusnicholaus.github.io/Leaderboard/',
-    link_to_source: 'https://github.com/jenadiusnicholaus/Leaderboard',
-  },
-
-  {
-    id: 1,
-    name: 'Lead Boarder',
-    description: 'The leaderboard website displays scores submitted by different players, it meant a play gets unique id for the Game session and from there can be allowed to play the game and to do that i used Leaderboard API using JavaScript async and await and add simple styling.',
-    featured: 'jenad',
-    image: 'assets/img/leaderBoard.png',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascripts',
-      'jest',
-    ],
-    link_to_live_version: 'https://jenadiusnicholaus.github.io/Leaderboard/',
-    link_to_source: 'https://github.com/jenadiusnicholaus/Leaderboard',
-  },
-
-  {
-    id: 2,
-    name: 'To Do List Project',
-    description: '"To-do list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete. I will build a simple website that allows for doing that, and i will do it using ES6 and Webpack!',
-    featured: 'jenad',
-    image: 'assets/img/mytodo2.png',
-    technologies: [
-      'Html',
-      'Css',
-      'Javascripts',
-      'jest',
-    ],
-    link_to_live_version: 'https://jenadiusnicholaus.github.io/To-Do-list/',
-    link_to_source: 'https://github.com/jenadiusnicholaus/To-Do-list',
-  },
-
-  {
-    id: 3,
-    name: 'Wora',
-    description: 'To-do list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete. I will build a simple website that allows for doing that, and i will do it using ES6 and Webpack!',
-    featured: 'jenad',
-    image: 'assets/img/wora.png',
-    technologies: [
-      'flutter',
-      'Dart',
-      'firebase',
-    ],
-    link_to_live_version: 'https://jenadiusnicholaus.github.io/To-Do-list/',
-    link_to_source: 'https://github.com/jenadiusnicholaus/To-Do-list',
-  },
-
-];
-
+import Api from "./api.js";
 const modal = document.getElementById('project-modal');
 const workDescription = document.getElementById('work-description');
 const workTitle = document.getElementById('title');
 const liveLink = document.getElementById('live-link');
 const source = document.getElementById('work-source');
 
-document.getElementById('recentProjectBtn').onclick = () => {
+document.getElementById('recentProjectBtn').onclick =  async () => {
+   const projectDataTwo = await Api.getProjects()
   const recenwork = projectDataTwo[0];
   modal.style.display = 'block';
   workTitle.textContent = recenwork.name;
@@ -80,8 +16,8 @@ document.getElementById('recentProjectBtn').onclick = () => {
   document.getElementById('project-image').setAttribute('src', recenwork.image);
 };
 
-document.getElementById('recent-work-image').setAttribute('src', projectDataTwo[0].image);
-
+// document.getElementById('recent-work-image').setAttribute('src', projectDataTwo[0].image);
+// close modal
 document.getElementsByClassName('close')[0].onclick = () => {
   modal.style.display = 'none';
 };
@@ -93,12 +29,14 @@ window.onclick = (event) => {
 };
 
 // set links on click
-document.getElementById('work-card-container').addEventListener('click', (event) => {
+document.getElementById('work-card-container').addEventListener('click', async (event) => {
   const isButton = event.target.nodeName === 'BUTTON';
 
   if (!isButton) {
+    console.log(isButton)
     return;
   }
+  const projectDataTwo = await Api.getProjects()
 
   projectDataTwo.forEach((item) => {
     if (parseInt(event.target.value, 10) === item.id) {
