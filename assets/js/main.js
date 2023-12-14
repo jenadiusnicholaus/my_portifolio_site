416d5e4e680c1af4cae229aa0d6e2adc5a96ff7e
jenadiusnicholaus <ucode888@gmail.com>
2023-12-14 17:49:02 +0300

    fix ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,70 @@
-import Api from './api.js';
-
+import Api from "./api.js";
 
 const logoToggleClass = (element, toggleClass) => {
   element.classList.toggle(toggleClass);
 };
 
 // create an hambarger menu
-document.querySelectorAll('.hamburger-mobile-icon').forEach((hamburgIcon) => {
-  hamburgIcon.addEventListener('click', () => {
-    hamburgIcon.classList.toggle('open');
-    logoToggleClass(document.querySelector('.logo'), 'dark-logo');
+document.querySelectorAll(".hamburger-mobile-icon").forEach((hamburgIcon) => {
+  hamburgIcon.addEventListener("click", () => {
+    hamburgIcon.classList.toggle("open");
+    logoToggleClass(document.querySelector(".logo"), "dark-logo");
   });
 });
 
-window.addEventListener('load', async () => {
-  const workCardContainer = document.getElementById('work-card-container');
-  const recentworktitle = document.getElementById('recentworktitle');
-  const recentworkdesc = document.getElementById('recentworkdesc');
-  const img = document.getElementById('recent-work-image');
-  const techOne = document.getElementById('tech-1');
-  const techTwo = document.getElementById('tech-2');
-  const techThree = document.getElementById('tech-3');
-  workCardContainer.innerHTML = '';
-  let htmlCard = '';
+// createBTN
+// const createBtn = () => {
+//   // <button id="recentProjectBtn" type="button" class="btn">
+//   //   See Project
+//   // </button>;
+//   const btn = document.createElement("button");
+//   btn.setAttribute("id", "recentProjectBtn");
+//   btn.setAttribute("type", "button");
+//   btn.setAttribute("class", "btn");
+//   btn.textContent = "See Project";
+//   btn.style.display = "block";
+//   btn.addEventListener("click", async (event) => {
+//     const projectDataTwo = await Api.getProjects();
+//     const recenwork = projectDataTwo[projectDataTwo.length - 1];
+//     modal.style.display = "block";
+//     workTitle.textContent = recenwork.name;
+//     workDescription.textContent = recenwork.description;
+//     liveLink.setAttribute("href", recenwork.link_to_live_version);
+//     source.setAttribute("href", recenwork.link_to_source);
+//     document
+//       .getElementById("project-image")
+//       .setAttribute("src", recenwork.image);
+//   });
+//   return btn;
+// };
+
+window.addEventListener("load", async () => {
+  const workCardContainer = document.getElementById("work-card-container");
+  const recentworktitle = document.getElementById("recentworktitle");
+  const recentworkdesc = document.getElementById("recentworkdesc");
+  const img = document.getElementById("recent-work-image");
+  const techOne = document.getElementById("tech-1");
+  const techTwo = document.getElementById("tech-2");
+  const techThree = document.getElementById("tech-3");
+  workCardContainer.innerHTML = "";
+  let htmlCard = "";
   const projectData = await Api.getProjects();
 
   // Update recent project dynamically
   recentworktitle.textContent = projectData[projectData.length - 1].name;
-  img.setAttribute('src', projectData[projectData.length - 1].image);
+  img.setAttribute("src", projectData[projectData.length - 1].image);
   recentworkdesc.textContent = projectData[projectData.length - 1].description;
   // [techOne.value, techTwo.value, techThree.value] = projectData[0].technologies;
 
   // get data from  local storage
-  const retrievedObject = localStorage.getItem('contacts_info');
+  const retrievedObject = localStorage.getItem("contacts_info");
   const parseJson = JSON.parse(retrievedObject);
 
-
-  const newdata = projectData.sort((a, b) => projectData.indexOf(b) - projectData.indexOf(a));
+  const newdata = projectData.sort(
+    (a, b) => projectData.indexOf(b) - projectData.indexOf(a)
+  );
   for (let index = 0; index < newdata.length; index += 1) {
     const project = newdata[index];
-
-
 
     htmlCard += `<div class="card-container">
     <div class="work-card">
@@ -64,7 +88,21 @@ window.addEventListener('load', async () => {
     </div>`;
   }
 
+  document.getElementById("recentProjectBtn").style.display = "block";
+  //.shimmer-text
+  const shimmerText = document.querySelectorAll(".shimmer-text");
+  shimmerText.forEach((text) => {
+    text.style.display = "none";
+  });
+
+  // let buttons = document.getElementsByClassName("btn-see-project");
+  // let newElement = createBtn(); // Assuming createBtn() returns a Node
+
+  // for (let i = 0; i < buttons.length; i++) {
+  //   buttons[i].appendChild(newElement.cloneNode(true));
+  // }
+
   workCardContainer.innerHTML = htmlCard;
-  const recent_works_card = document.querySelector('.work-card');
+  const recent_works_card = document.querySelector(".work-card");
   // recent_works_card.style.background= "linear-gradient(180.45deg, rgba(38, 38, 38, 0) 0.75%, rgba(34, 32, 32, 0.184) 61.94%), url('./img/maskimage.png')";
 });
--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,52 +1,56 @@
 import Api from "./api.js";
-const modal = document.getElementById('project-modal');
-const workDescription = document.getElementById('work-description');
-const workTitle = document.getElementById('title');
-const liveLink = document.getElementById('live-link');
-const source = document.getElementById('work-source');
+const modal = document.getElementById("project-modal");
+const workDescription = document.getElementById("work-description");
+const workTitle = document.getElementById("title");
+const liveLink = document.getElementById("live-link");
+const source = document.getElementById("work-source");
 
-document.getElementById('recentProjectBtn').onclick = async () => {
-  const projectDataTwo = await Api.getProjects()
+document.getElementById("recentProjectBtn").onclick = async () => {
+  const projectDataTwo = await Api.getProjects();
   const recenwork = projectDataTwo[projectDataTwo.length - 1];
-  modal.style.display = 'block';
+  modal.style.display = "block";
   workTitle.textContent = recenwork.name;
   workDescription.textContent = recenwork.description;
-  liveLink.setAttribute('href', recenwork.link_to_live_version);
-  source.setAttribute('href', recenwork.link_to_source);
-  document.getElementById('project-image').setAttribute('src', recenwork.image);
+  liveLink.setAttribute("href", recenwork.link_to_live_version);
+  source.setAttribute("href", recenwork.link_to_source);
+  document.getElementById("project-image").setAttribute("src", recenwork.image);
 };
 
 // document.getElementById('recent-work-image').setAttribute('src', projectDataTwo[0].image);
 // close modal
-document.getElementsByClassName('close')[0].onclick = () => {
-  modal.style.display = 'none';
+document.getElementsByClassName("close")[0].onclick = () => {
+  modal.style.display = "none";
 };
 
 window.onclick = (event) => {
   if (event.target === modal) {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   }
 };
 
 // set links on click
-document.getElementById('work-card-container').addEventListener('click', async (event) => {
-  const isButton = event.target.nodeName === 'BUTTON';
+document
+  .getElementById("work-card-container")
+  .addEventListener("click", async (event) => {
+    const isButton = event.target.nodeName === "BUTTON";
 
-  if (!isButton) {
-    console.log(isButton)
-    return;
-  }
-  const projectDataTwo = await Api.getProjects()
-
-  projectDataTwo.forEach((item) => {
-    if (parseInt(event.target.value, 10) === item.id) {
-      modal.style.display = 'block';
-      workTitle.textContent = item.name;
-      workDescription.textContent = item.description;
-      liveLink.setAttribute('href', item.link_to_live_version);
-      source.setAttribute('href', item.link_to_source);
-      document.getElementById('project-image').setAttribute('src', item.image);
+    if (!isButton) {
+      console.log(isButton);
+      return;
     }
-    return item;
+    const projectDataTwo = await Api.getProjects();
+
+    projectDataTwo.forEach((item) => {
+      if (parseInt(event.target.value, 10) === item.id) {
+        modal.style.display = "block";
+        workTitle.textContent = item.name;
+        workDescription.textContent = item.description;
+        liveLink.setAttribute("href", item.link_to_live_version);
+        source.setAttribute("href", item.link_to_source);
+        document
+          .getElementById("project-image")
+          .setAttribute("src", item.image);
+      }
+      return item;
+    });
   });
-});
